f81efafbed2877b77731cc3c38eaf2887e490bca
salonigupta14 <37844488+salonigupta14@users.noreply.github.com>
2018-10-27 13:00:49 +0530

    Update 3.cpp

--- 3.cpp
+++ 3.cpp
@@ -12,10 +12,12 @@
 #include "iostream"
 #include "cstring"
 #include <ncurses.h>
-#include <stack>
 #include <vector>
+#define ll long long
+#define ss stack
+#include<bits/stdc++.h>
 using namespace std;
- 
+ //comment should be there
 void up(long long int arrarrary[])
 {
     for (long long int i = 0; i < 3; ++i)
